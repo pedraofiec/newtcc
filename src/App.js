a37db652879739648d156e68816923b64a687b88
Pedro <pedro41691@fiec.edu.br>
2025-09-01 20:10:42 -0300

    homescreen funcional botoes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 // Importa os componentes de cada funcionalidade
-import HomeScreen from './features/home/components/HomeScreen'; // <-- Apenas para testar o HomeScreen
+import HomeScreen from './features/home/components/HomeScreen';
 import SplashScreen from './features/splash/SplashScreen';
 import LoginForm from './features/splash/components/LoginForm';
 import UserTypeSelection from './features/splash/components/UserTypeSelection';
@@ -17,22 +17,17 @@ import RegisterStudent from './features/cadastro/components/RegisterStudent';
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          {/* Para testar o HomeScreen, ele é a primeira rota */}
-          <Route path="/" element={<HomeScreen />} />
-
-          {/* As outras rotas permanecem para quando você precisar delas */}
-          <Route path="/splash" element={<SplashScreen />} />
-          <Route path="/login" element={<LoginForm />} />
-          <Route path="/forgot-password" element={<ForgotPassword />} />
-          <Route path="/register" element={<UserTypeSelection />} />
-          <Route path="/register/responsible" element={<RegisterResponsible />} />
-          <Route path="/register/driver" element={<RegisterDriver />} />
-          <Route path="/register/school" element={<RegisterSchool />} />
-          <Route path="/register/student" element={<RegisterStudent />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/splash" element={<SplashScreen />} />
+        <Route path="/login" element={<LoginForm />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/register" element={<UserTypeSelection />} />
+        <Route path="/register/responsible" element={<RegisterResponsible />} />
+        <Route path="/register/driver" element={<RegisterDriver />} />
+        <Route path="/register/school" element={<RegisterSchool />} />
+        <Route path="/register/student" element={<RegisterStudent />} />
+      </Routes>
     </div>
   );
 }
