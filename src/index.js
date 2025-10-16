c8753c9848979ce7a67b1222be1b296abf2d9d74
Caio <caio41667@fiec.edu.br>
2025-10-16 19:47:32 -0300

    firebase

--- src/index.js
+++ src/index.js
@@ -21,3 +21,7 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("/firebase-messaging-sw.js");
+}
